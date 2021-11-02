01bc076d7de5051e8f53155041fcb748dcf2ff7c
aomarchenko <80917411+aomarchenko@users.noreply.github.com>
2021-11-02 19:56:33 +0000

    Deploying to gh-pages from @ aomarchenko/alisamassage@46fde36274ad8ed5758f2f9f1fc612b06dd84c9d 🚀

--- js.22facfe0.js
+++ js.22facfe0.js
@@ -17,6 +17,6 @@ var r=require("./bundle-url").getBundleURL;function e(r){Array.isArray(r)||(r=[r
 },{"./bundle-url":"FheM"}],"A3BY":[function(require,module,exports) {
 module.exports=function(t){return fetch(t).then(function(t){return t.text()})};
 },{}],0:[function(require,module,exports) {
-var b=require("TUK3");b.register("html",require("A3BY"));b.load([["price-list.80055a70.html","dRHv"],["home.e3b8ab2c.html","STfV"],["news.567ce659.html","ssVt"],["about-me.60a0e79b.html","QGLI"],["video.16f1f804.html","j4hd"],["gift-sertificate.f828a15d.html","Blag"],["contact.1af4d41b.html","ePGw"],["massages.540b8f3d.html","pmIJ"],["wellness.09df09c3.html","PxdA"],["body-estetik-massage.baebb672.html","TA7Z"],["gezichtsmassage.c2b59acf.html","aZXa"],["spa-pedicure.5a7e2740.html","WVun"],["body-detox-voetenbad.9a1ef002.html","cGTr"],["oorkaarsen.eab06e71.html","Pia6"]]).then(function(){require("QvaY");});
+var b=require("TUK3");b.register("html",require("A3BY"));b.load([["price-list.80055a70.html","dRHv"],["home.f40fd929.html","STfV"],["news.567ce659.html","ssVt"],["about-me.60a0e79b.html","QGLI"],["video.16f1f804.html","j4hd"],["gift-sertificate.f828a15d.html","Blag"],["contact.1af4d41b.html","ePGw"],["massages.540b8f3d.html","pmIJ"],["wellness.09df09c3.html","PxdA"],["body-estetik-massage.baebb672.html","TA7Z"],["gezichtsmassage.c2b59acf.html","aZXa"],["spa-pedicure.5a7e2740.html","WVun"],["body-detox-voetenbad.9a1ef002.html","cGTr"],["oorkaarsen.eab06e71.html","Pia6"]]).then(function(){require("QvaY");});
 },{}]},{},[0], null)
-//# sourceMappingURL=/alisamassage/js.18af135b.js.map
+//# sourceMappingURL=/alisamassage/js.22facfe0.js.map
